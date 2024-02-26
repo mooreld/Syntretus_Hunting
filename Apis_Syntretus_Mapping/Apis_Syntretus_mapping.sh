e08cb15049d9a3d4e2079185a215ea3c1bfaac62
mooreld <160888852+mooreld@users.noreply.github.com>
2024-02-26 14:52:56 -0600

    Add files via upload

---
+++ Apis_Syntretus_Mapping/Apis_Syntretus_mapping.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --cpus-per-task 24
+
+cd /<directory>/
+
+module load ncbi-sra/3.0.2
+module load BBmap
+
+file_path="Apis_cerana_SRA.txt"
+
+sed -i 's/\r$//' "$file_path"
+
+while IFS= read -r line || [[ -n "$line" ]]
+do
+ fasterq-dump "$line" --split-files
+ bbmap.sh in1="$line"_1.fastq in2="$line"_2.fastq ref=Apis_Syntretus_mitochondria.fasta covstats=/coverage/"$line"_coverage.txt fast minratio=0.85 maxindel=1 strictmaxindel=t outm=/bbmap_output/"$line".fastq 
+ rm "$line"_1.fastq
+ rm "$line"_2.fastq
+done < "$file_path"
+
